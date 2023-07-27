f916399dd35792016c91dd3a00cd010850f09f89
Mark Johnston <git@mhj.ca>
2023-07-26 20:40:55 -0500

    Problem 0

---
+++ protohackers/src/main.rs
@@ -0,0 +1,46 @@
+use clap::{Parser, ValueEnum};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+#[derive(Copy, Clone, Debug, ValueEnum)]
+enum Problem {
+    SmokeTest,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Host and port to listen on
+    #[arg(short, long, default_value = "0.0.0.0:32767")]
+    listen: SocketAddr,
+
+    /// Problem to run
+    #[arg(short, long, default_value = "smoke-test")]
+    problem: Problem,
+}
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "debug");
+    }
+
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let args = Args::parse();
+
+    info!(listener=%args.listen, "listening");
+
+    let listener = TcpListener::bind(args.listen).await?;
+
+    match args.problem {
+        Problem::SmokeTest => smoke_test::serve(listener)
+    }.await;
+
+    Ok(())
+}
---
+++ smoke-test/src/lib.rs
@@ -0,0 +1,47 @@
+use std::future::Future;
+use std::net::SocketAddr;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{debug, info, warn};
+
+pub async fn serve(listener: TcpListener) {
+    info!("starting");
+    loop {
+        match listener.accept().await {
+            Ok((stream, addr)) => {
+                info!(client=%addr, "connection received");
+                tokio::spawn(handle(stream, addr));
+            }
+            Err(e) => {}
+        }
+    }
+}
+
+async fn handle(mut stream: TcpStream, addr: SocketAddr) {
+    let mut buf = Vec::with_capacity(1024);
+    let mut connected = true;
+    while connected {
+        match stream.read_buf(&mut buf).await {
+            Ok(n) => {
+                debug!(client=%addr, bytes=n, data=%String::from_utf8_lossy(&buf), "read ok");
+                if n == 0 { connected = false; }
+
+                match stream.write_all(&buf).await {
+                    Ok(()) => {
+                        debug!(client=%addr, bytes=buf.len(), "write ok");
+                        buf.clear();
+
+                    }
+                    Err(e) => {
+                        warn!(client=%addr, error=%e, "write failed");
+                        break;
+                    }
+                }
+            }
+            Err(e) => {
+                warn!(client=%addr, error=%e, "read failed");
+            }
+        }
+    }
+    info!(client=%addr, "disconnect");
+}
